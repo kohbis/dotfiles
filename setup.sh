8e9bdde12c56ec3e9a9a98adf3185f975b70734b
kohbis <18735471+kohbis@users.noreply.github.com>
2020-05-09 13:12:49 +0900

    add setup.sh options

--- setup.sh
+++ setup.sh
@@ -1,8 +1,44 @@
 #!/bin/bash
 
-DOT_FILES=(.bashrc .bash_profile .vimrc .zshrc .tmux.conf)
+usage_exit() {
+  echo "Usage: `basename $0` [-d] [-s] [-u]" 1>&2
+  exit 1
+}
 
-for file in ${DOT_FILES[@]}; do
-  ln -s $HOME/workspace/settings/dotfiles/$file $HOME/$file
+DOT_FILES=(bashrc bash_profile vimrc zshrc tmux.conf)
+
+create_symbolic_link() {
+  echo "create symbolic links"
+  for file in ${DOT_FILES[@]}; do
+    ln -s $HOME/workspace/settings/dotfiles/$file $HOME/.$file
+  done
+}
+
+remove_symbolic_link() {
+  echo "remove symbolic links"
+  for file in ${DOT_FILES[@]}; do
+    unlink $HOME/.$file
+  done
+}
+
+recreate_symbolic_link() {
+  echo "recreate symbolick links"
+  remove_symbolic_link
+  create_symbolic_link
+}
+
+if [ $# -ne 1 ]; then
+  usage_exit
+fi
+
+while getopts su OPT
+do
+  case $OPT in
+    "d" ) remove_symbolic_link ;;
+    "u" ) recreate_symbolic_link ;;
+    "s" ) create_symbolic_link ;;
+    * ) usage_exit ;;
+  esac
 done
 
+echo "finish!"
