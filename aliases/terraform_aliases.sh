391ddefe3bbf72d334c7f2dfc265ffaf3a147121
kohbis <18735471+kohbis@users.noreply.github.com>
2022-09-04 15:51:59 +0900

    update tf alias

--- aliases/terraform_aliases.sh
+++ aliases/terraform_aliases.sh
@@ -4,8 +4,8 @@
 
 alias tf='terraform'
 alias tfa='terraform apply'
-alias tfd='terraform destroy'
+# alias tfd='terraform destroy'
 alias tfi='terraform init'
 alias tfp='terraform plan'
-alias tfw='terraform workspace'
+# alias tfw='terraform workspace'
 
