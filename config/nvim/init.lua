ef77080e09889d57a009d6bde05748c695a25fc1
kohbis <18735471+kohbis@users.noreply.github.com>
2025-02-19 01:15:24 +0900

    feat: 🎸 Telescope

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -81,6 +81,16 @@ require('packer').startup({
     -- Color Scheme
     use 'morhetz/gruvbox'
 
+    -- Telescope
+    use {
+      'nvim-telescope/telescope.nvim',
+      requires = {
+        'nvim-lua/plenary.nvim',
+        { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
+        -- brew install ripgrep
+      },
+    }
+
     use {
       'lukas-reineke/indent-blankline.nvim',
       config = function()
@@ -499,6 +509,13 @@ vim.keymap.set('n', '<leader>p', '<cmd>CtrlP<CR>', { noremap = true })
 vim.keymap.set('n', '<C-n>', '<cmd>NvimTreeToggle<CR>', { noremap = true })
 vim.keymap.set('n', '<leader>n', '<cmd>NvimTreeFindFile<CR>', { noremap = true })
 vim.keymap.set('n', '<leader>r', '<cmd>NvimTreeRefresh<CR>', { noremap = true })
+
+-- Telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
 -- #####
 -- Color
